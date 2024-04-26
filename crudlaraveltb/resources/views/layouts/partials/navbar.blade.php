2bfd52f177c7354a14adb67264020bbd31c623ba
Diego <diego.lopes.tinfem2021@gmail.com>
2024-04-26 18:47:48 -0300

    testeeeeee

--- crudlaraveltb/resources/views/layouts/partials/navbar.blade.php
+++ crudlaraveltb/resources/views/layouts/partials/navbar.blade.php
@@ -1,3 +1,4 @@
+<!--
 <header class="p-3 bg-dark text-white">
   <div class="container">
 	<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -20,3 +21,47 @@
 	</div>
   </div>
 </header>
+-->
+
+<div>
+	<!--Off Canvas -->
+	<nav class="navbar navbar-expand-lg fixed-top">
+		<div class="container-fluid">
+			<!--Logo IFPR -->
+		  <a class="navbar-brand me-auto" href="#"> <img src="layouts\images\Nasa_blue_marble.jpg" alt="logo ifpr"></a>
+		  
+		  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+			<div class="offcanvas-header">
+			  <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Logo</h5>
+			  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+			</div>
+			<div class="offcanvas-body">
+			  <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+				<li class="nav-item">
+				  <a class="nav-link mx-lg-2 active " aria-current="page" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link mx-lg-2" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link mx-lg-2" href="#">Link</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link mx-lg-2" href="#">Link</a>
+				  </li>
+				  <li class="nav-item">
+					<a class="nav-link mx-lg-2" href="#">Link</a>
+				  </li>
+			  </ul>
+			</div>
+		  </div>
+
+		  <a href="#" class="login-button">Login</a>
+		  <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+		</div>
+	  </nav>
+
+
+</div>
